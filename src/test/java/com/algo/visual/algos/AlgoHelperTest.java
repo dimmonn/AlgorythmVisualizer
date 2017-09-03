5d0a5b35993bee9639744dfa6c59b48bfd99f3f3
Dmytro <nhfdf@ukr.net>
2017-09-03 13:05:00 +0200

    adding some tests

--- src/test/java/com/algo/visual/algos/AlgoHelperTest.java
+++ src/test/java/com/algo/visual/algos/AlgoHelperTest.java
@@ -15,20 +15,34 @@ class AlgoHelperTest {
     @BeforeEach
     void setUp() {
         _sortable = new AtomicInteger[6];
-        for (int i = 0; i < _sortable.length; i++) {
-            AtomicInteger _tmp = new AtomicInteger();
-            _tmp.set(i);
-            _sortable[i] = _tmp;
-        }
+        AtomicInteger _tmp0 = new AtomicInteger();
+        _tmp0.set(4);
+        AtomicInteger _tmp1 = new AtomicInteger();
+        _tmp1.set(1);
+        AtomicInteger _tmp2 = new AtomicInteger();
+        _tmp2.set(2);
+        AtomicInteger _tmp3 = new AtomicInteger();
+        _tmp3.set(6);
+        AtomicInteger _tmp4 = new AtomicInteger();
+        _tmp4.set(20);
+        AtomicInteger _tmp5 = new AtomicInteger();
+        _tmp5.set(0);
+        _sortable[0] = _tmp0;
+        _sortable[1] = _tmp1;
+        _sortable[2] = _tmp2;
+        _sortable[3] = _tmp3;
+        _sortable[4] = _tmp4;
+        _sortable[5] = _tmp5;
+
         algoHelper = new AlgoHelper(new Visualizer());
     }
 
     @Test
     void mergeSort() {
         algoHelper.mergeSort(_sortable);
-        Assert.assertEquals(5, _sortable[0].get());
-        Assert.assertEquals(4, _sortable[1].get());
-        Assert.assertEquals(3, _sortable[2].get());
+        Assert.assertEquals(20, _sortable[0].get());
+        Assert.assertEquals(6, _sortable[1].get());
+        Assert.assertEquals(4, _sortable[2].get());
         Assert.assertEquals(2, _sortable[3].get());
         Assert.assertEquals(1, _sortable[4].get());
         Assert.assertEquals(0, _sortable[5].get());
